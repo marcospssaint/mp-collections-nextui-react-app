f06e8dce9e0cd4f0dd6718d30fee9cf395d114f3
Marcos Paulo Lima <marcospssaint@gmail.com>
2024-08-27 19:24:08 +0100

    Config gh-pages part 4

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,6 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import { HashRouter } from 'react-router-dom';
 
-import './output.css';
-
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <HashRouter>
--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/mp-collections-nextui-react-app/",
-  build:{
-    outDir: "dist"
-  }
+  base: "./",
 })
