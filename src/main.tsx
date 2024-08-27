d38f43eb7fc570b996eba32d7208ebbad49be27a
Marcos Paulo Lima <marcospssaint@gmail.com>
2024-08-27 18:56:41 +0100

    Config gh-pages parte 2

--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,12 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import { HashRouter } from 'react-router-dom';
 
+import './output.css';
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <HashRouter>
-      <main className={`${false ? 'dark' : ''} `}>
+      <main>
         <App />
       </main>
     </HashRouter>
