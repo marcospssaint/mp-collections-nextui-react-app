b9be6de49b49264a7f2227c643a2732e2aa6f3d8
Marcos Paulo Lima <marcospssaint@gmail.com>
2024-08-27 19:14:13 +0100

    Config gh-pages part 3

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/mp-collections-nextui-react-app/"
+  base: "/mp-collections-nextui-react-app/",
+  build:{
+    outDir: "dist"
+  }
 })
