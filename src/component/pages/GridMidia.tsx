3419305a6e883314c2743f4ee2d7b3a1cc6e0f25
Marcos Paulo Lima <marcospssaint@gmail.com>
2024-12-29 23:20:57 +0000

    Feature visible component GridMidia

--- src/component/pages/GridMidia.tsx
+++ src/component/pages/GridMidia.tsx
@@ -1,6 +1,6 @@
 import { Card, CardFooter, Image, Pagination } from "@nextui-org/react";
-import { IMidia } from "../../data/midia";
-import { imageModified, textMidia } from "../../utils/utils";
+import { IMidia, statusByMidia } from "../../data/midia";
+import { imageModified, statusColorMap, textMidia } from "../../utils/utils";
 
 interface GridMidiaProps {
     items: any[],
@@ -23,35 +23,38 @@ export const GridMidiaComponent = ({
         <div className="grid pt-4" >
             <div className="grid sm-app:grid-cols-2 sm-app:grid-cols-3 ml-app:grid-cols-4 lg-app:grid-cols-6 lg-app:grid-cols-10 xl-app:grid-cols-10 xl-app:grid-cols-12 gap-2">
                 {items.map((item: any, index: number) => (
-                    <div key={`${index}_card`} className="p-1">
-                        <Card
-                            isFooterBlurred
-                            radius="lg"
-                            className="border-none"
-                            isPressable
-                            onPress={() => {
-                                setMidiaSelected(item);
-                                onOpen();
-                            }}
-                        >
-                            <Image
-                                key={`image_modal_${item?.id}`}
-                                className="rounded shadow-md w-full h-auto object-cover"
-                                width='130px'
-                                height='200px'
-                                src={imageModified(item?.img)}
-                            />
-                            <CardFooter className="justify-between py-1 absolute bottom-1 ml-1 z-10" style={{
-                                background: 'linear-gradient(180deg,transparent,rgba(0,0,0,.8))'
-                            }}>
-                                <div>
-                                    <span style={{ color: 'rgb(255 255 255/var(--tw-text-opacity,1))', fontSize: '.875rem', fontWeight: 400 }}>
-                                        {item.year} ·  {textMidia(item.title)}
-                                    </span>
-                                </div>
-                            </CardFooter>
-                        </Card>
-                    </div>
+                    <Card
+                        key={`${index}_card`}
+                        isFooterBlurred
+                        radius="lg"
+                        className="border-none"
+                        isPressable
+                        onPress={() => {
+                            setMidiaSelected(item);
+                            onOpen();
+                        }}
+                    >
+                        <Image
+                            removeWrapper
+                            key={`image_modal_${item?.id}`}
+                            className="z-0 w-full h-full object-cover"
+                            width='180px'
+                            height='280px'
+                            src={imageModified(item?.img)}
+                        />
+                        {
+                            statusColorMap[statusByMidia(item)] == 'success' &&
+                            <div className="absolute bg-green-500 top-0 right-0 rounded-full p-3 m-2 shadow-sm w-6 h-6" />
+                        }
+                        <CardFooter className="justify-between py-1 absolute bottom-1 ml-1 z-10" style={{
+                            background: 'linear-gradient(180deg,transparent,rgba(0,0,0,.8))'
+                        }}>
+                            <span style={{ color: 'rgb(255 255 255/var(--tw-text-opacity,1))', fontSize: '.875rem', fontWeight: 400 }}>
+                                {item.year} ·  {textMidia(item.title)}
+                            </span>
+                        </CardFooter>
+                    </Card>
+
                 ))}
             </div>
             <div className="flex flex-cols w-full justify-center gap-4 py-2">
